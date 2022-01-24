9c54cedf6ad874cfaa5c60059803d138ed06bbb6
BPPSwebsite <mattiashakansson06@gmail.com>
2022-01-24 11:48:47 +0100

    first commit

---
+++ CoinGameJS.js
@@ -0,0 +1,10 @@
+
+console.log("babaganush");
+
+var age = "Twenty one";
+var online = true
+var helium;
+
+console.log("Your age is",age);
+console.log("Are you online?", online);
+console.log("This is a", helium)
